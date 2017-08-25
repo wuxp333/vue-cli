ded5bbc1a1ffbf7e40ccdd13630ca963797b0253
zhoujianxiang <zhoujianxiang@hdfax.com>
2017-08-25 10:12:39 +0800

    update

--- vue-cli-webpack-simple/src/App.vue
+++ vue-cli-webpack-simple/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="hello">
+  <div id="app">
+    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -7,8 +8,6 @@
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
@@ -22,7 +21,7 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -31,8 +30,16 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 h1, h2 {
   font-weight: normal;
 }
